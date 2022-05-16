438434c1d1a4b0469b46425a1ddf557f757e2d16
Agustin Morinigo <agustinmorinigo@MacBook-Air-de-CRUCE.local>
2022-05-16 00:15:01 -0300

    finished main slider

--- src/js/components/slider.js
+++ src/js/components/slider.js
@@ -1,1 +1,14 @@
-console.log( 'Slider' );
+const Slider = () => {
+
+    const swiper = new Swiper('.swiper', {
+        loop: true,
+        pagination: {
+            el: '.swiper-pagination',
+            clickable: true
+        }
+    });
+      
+
+};
+
+export default Slider;
--- src/js/index.js
+++ src/js/index.js
@@ -1,5 +1,7 @@
 import Navbar from './common/navbar';
 import Footer from './common/footer';
+import Slider from './components/slider';
 
 Navbar();
+Slider();
 Footer();
